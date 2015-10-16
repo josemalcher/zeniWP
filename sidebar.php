7b1e8d4e73baf2d4f620822fecf1ddf50c09215a
José Stélio Malcher Junior <malcher.malch@gmail.com>
2015-10-16 19:24:45 -0300

    Aula 24 - Criação de Temas para Wordpress - Treinamento/Atualização -SideBar

---
+++ sidebar.php
@@ -0,0 +1,29 @@
+
+<!-- sidebar -->
+<aside id="sidebar">
+
+    <ul>
+        <li class="block">
+            <h4>CATEGORIAS</h4>
+            <ul>
+                <?php wp_list_categories(
+                        array(
+                          'title_li'   => '',
+                          'exclude'    => '1',
+                        )
+                        ); ?>
+            </ul>
+        </li>
+
+        <li class="block">
+            <h4>ARQUIVOS</h4>
+            <ul>
+                <?php wp_get_archives();?>
+            </ul>
+        </li>
+
+    </ul>
+
+    <em id="corner"></em>
+</aside>
+<!-- ENDS sidebar -->
